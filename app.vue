247871d8a7c3c6c30999b3431a0a7fc199f338b3
spectralo <arthur.hardy@mageekbox.net>
2024-08-03 16:58:35 +0200

    Base TUI!

--- app.vue
+++ app.vue
@@ -1,6 +1,68 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="terminalcontainer">
+    <p class="history" ref="history"></p>
+    <div class="editable">
+      <p class="prompt">[{{ path }}] </p>
+      <div class="line"  ref="line" contenteditable="true"></div>
+    </div>
+    
   </div>
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background-color: oklch(0% 0 0);
+  font-family: JetBrains Mono, monospace;
+  font-size: 2em;
+}
+.terminalcontainer {
+  color: oklch(85.29% 0.05 225.93);
+}
+.editable {
+  display: flex;
+  flex-direction: row;
+  height: fit-content;
+  margin: 0;
+  outline: 0px solid transparent;
+  border: 0px solid transparent;
+}
+.line {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+  margin: 0;
+  margin-left: 0.5em;
+}
+*:focus {
+    outline: none;
+}
+
+
+</style>
+
+<script lang="js" setup>
+let path = '~'
+import { ref, onMounted } from 'vue'
+const line = ref(null)
+
+onMounted(() => {
+  line.value.focus()
+  line.value.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      const input = line.value.innerText
+      line.value.innerText = ''
+      const history = document.querySelector('.history')
+      history.innerHTML += `<p>[${path}] ${input}</p>`
+    }
+  })
+})
+
+</script>
+
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    ["@nuxtjs/google-fonts", 
+      { 
+        families: { 
+          Roboto: [400, 700, 900],
+          "JetBrains Mono": [500, 700]
+
+        } 
+      }  
+    ]
+  ]
 })
