0061d671821bc819509c2b9240e222a74be44669
spectralo <arthur.hardy@mageekbox.net>
2024-08-03 23:28:58 +0200

    Made some changes

--- app.vue
+++ app.vue
@@ -14,14 +14,26 @@
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  letter-spacing: 0px;
 }
 body {
   background-color: oklch(0% 0 0);
-  font-family: JetBrains Mono, monospace;
-  font-size: 2em;
+  font-family: JetBrains Mono, monospace ;
+  font-size: 30px;
+  margin-top: 5%;
+  margin-bottom: 5%;
+  margin-left: 5%;
+  margin-right: 5%; 
 }
+
+pre {
+  font-family: JetBrains Mono, monospace ;
+  font-size: 30px;
+  color: oklch(80.15% 0.15 320.57);
+}
+
 .terminalcontainer {
-  color: oklch(85.29% 0.05 225.93);
+  color: oklch(80.15% 0.15 320.57);
 }
 .editable {
   display: flex;
@@ -44,6 +56,7 @@ body {
 }
 
 
+
 </style>
 
 <script lang="js" setup>
@@ -51,15 +64,87 @@ let path = '~'
 import { ref, onMounted } from 'vue'
 const line = ref(null)
 
+
 onMounted(() => {
+  const history = document.querySelector('.history')
+
+
+  function returnText(text) {
+    history.innerHTML += `<p><pre>${text}</pre></p>`
+  }
+
+
+  // Define commands
+
+  let commands = {
+    'help' : [help, 'Give you some hints', '1 : Command name (optional)'],
+  }
+
+  // Welcome message
+    
+  let ascii = [
+    "                               ",
+    "             ,%                ",
+    "            %%&##              ",
+    "        %%%%%%&////%%          ",
+    "        %%&&@@@##//%%          ",
+    "        //####%//////          ",
+    "&&&&##    ///(%////**    ##&&& ",
+    "%%&&&&%%  ////#////**  %%&&&&% ",
+    "%%//@@%%%%**///##//  %%%%@@/(% ",
+    "  %%////##//**///**//%%////%(  ",
+    "    %%////////*//**////////    ",
+    "      ////**//*%%//**////      "
+  ]
+
+  ascii.forEach(element => {
+    console.log(element)
+    returnText(`<p><pre>${element}</pre></p>`)
+  });
+  returnText(`<p><pre> </pre></p>`)
+  returnText(`<p><pre>Welcome to Spectralo's TUI website</pre></p>`)
+  returnText(`<p><pre>Type 'help' to get some hints ✨ </pre></p>`)
+
   line.value.focus()
   line.value.addEventListener('keydown', (e) => {
+    
     if (e.key === 'Enter') {
+      // Add the command to the history
       e.preventDefault()
       const input = line.value.innerText
+      
+      // Reset input line
       line.value.innerText = ''
+
+      // Break down the command to use it 
+      let breakedInput = input.split(' ')
+      let command = breakedInput[0]
+      let args = breakedInput.slice(1)
+      let options = []
+
+      args.forEach(element => {
+        let elementArray = element.split('')
+        if (elementArray[0] == '-') {
+          options.push(element)
+          args.splice(args.indexOf(element), 1)
+        }
+      });
+
+      console.log(command)
+      console.log(args)
+      console.log(options)
+
       const history = document.querySelector('.history')
       history.innerHTML += `<p>[${path}] ${input}</p>`
+
+      // Add output to the history
+      if (input === '') {
+        history.innerHTML += `<p></p>`
+      } else if (input === 'rm -rf /') {
+        window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+      } else {
+        history.innerHTML += `<p>Command not found: ${input}</p>`
+      }
     }
   })
 })
